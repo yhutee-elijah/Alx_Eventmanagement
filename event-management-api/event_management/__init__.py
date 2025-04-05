42de5140bd36e27d3a8c2a8795180718e6eb0358
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-04-05 19:48:20 +0100

    updated events

--- event-management-api/event_management/__init__.py
+++
@@ -1,3 +0,0 @@
-# The contents of the file /event-management-api/event-management-api/event_management/__init__.py
-
-# This file is intentionally left blank.
--- event-management-api/events/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Event, User
-
-admin.site.register(Event)
-admin.site.register(User)
